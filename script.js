1d005fef267c28df685daa41fb2c28675ee4aa8f
dhandatannu <tannudhanda04@gmail.com>
2024-02-06 10:47:30 +0530

    my digital

---
+++ script.js
@@ -0,0 +1,24 @@
+const hamburger = document.getElementById('hamburger'); 
+const menu = document.querySelector('.menu'); 
+
+hamburger.addEventListener('click', function () { 
+	const hamIcon = this.querySelector('.hamburger-icon'); 
+	const crossIcon = this.querySelector('.cross-icon'); 
+	if (hamIcon.style.display === "none") { 
+		hamIcon.style.display = "inline-block"
+		menu.style.display = "none"
+		crossIcon.style.display = "none"
+	} 
+	else { 
+		crossIcon.style.display = "inline-block"
+		hamIcon.style.display = "none"
+		menu.style.display = "block"
+	} 
+});
+
+
+
+function toggleContent(id) {
+    var content = document.getElementById(id);
+    content.style.display = (content.style.display === 'none' || content.style.display === '') ? 'block' : 'none';
+}
